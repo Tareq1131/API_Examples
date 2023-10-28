ea3f1b037615b5af28cd487c15c99b45049d14cc
Tareq1131 <tareqislam076@gmail.com>
2023-10-28 11:56:41 +0600

    Handle random user with nested API data

---
+++ js/forener.js
@@ -0,0 +1,24 @@
+const loadUser = () =>{
+    fetch('https://randomuser.me/api/?gender=female')
+    .then(res => res.json())
+    .then(data => display(data));
+}
+
+const display = user =>{
+ 
+ const nameTag = document.getElementById('name');
+ nameTag.innerHTML = user.results[0].name.title +' '+  user.results[0].name.first + ' '+ user.results[0].name.last
+
+ const genderTag = document.getElementById('gender');
+ genderTag.innerHTML = user.results[0].gender
+
+ const locationTag = document.getElementById('location');
+ locationTag.innerHTML = user.results[0].location.city
+
+ const imgTag = document.getElementById('img');
+ imgTag.src =user.results[0].picture.medium
+
+ console.log(user.results[0].name);
+}
+
+loadUser();
