56d01fae3a5a975376bb91f30ada811b30360a05
Tareq1131 <tareqislam076@gmail.com>
2023-10-29 00:34:02 +0600

    Create dynamic url based on click and display modal

--- js/meal.js
+++ js/meal.js
@@ -1,23 +1,23 @@
-const loadMeals = (searchText)=>{
-    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayMeals(data.meals));
-}
+const loadMeals = (searchText) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
 
-const displayMeals = meals=>{
-    //console.log(meals)
-     // 1. set container element
-    const mealsContainer = document.getElementById('meals-container');
-    mealsContainer.innerHTML = '';
-   
-    meals.forEach(meal =>{
-       console.log(meal); 
+const displayMeals = (meals) => {
+  //console.log(meals)
+  // 1. set container element
+  const mealsContainer = document.getElementById("meals-container");
+  mealsContainer.innerHTML = "";
+
+  meals.forEach((meal) => {
+    console.log(meal);
     //    2. create child for each element
-       const mealDiv = document.createElement('div');
-       mealDiv.classList.add('col');
+    const mealDiv = document.createElement("div");
+    mealDiv.classList.add("col");
     //    3.set content of the child
-    mealDiv.innerHTML =`
+    mealDiv.innerHTML = `
     <div class="card h-100">
         <img class="img1" src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -25,7 +25,7 @@ const displayMeals = meals=>{
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
            
             <!-- Button trigger modal -->
-            <button onClick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+            <button onClick="loadMealDetail2(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
              Details
             </button>
        
@@ -33,39 +33,52 @@ const displayMeals = meals=>{
         </div>
     
     
-    `
+    `;
     // 4. appendChild
     mealsContainer.appendChild(mealDiv);
-    })
-}
+  });
+};
 
-const searchMeals = () =>{
-    const searchText = document.getElementById('search-field').value;
-    console.log(searchText)
-    loadMeals(searchText)
-}
+const searchMeals = () => {
+  const searchText = document.getElementById("search-field").value;
+  console.log(searchText);
+  loadMeals(searchText);
+};
 
-const loadMealDetail = idMeal =>{
-    const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
- //console.log(idMeal)
- fetch(url)
- .then(res => res.json())
- .then(data => desplyMealDetails (data.meals[0]));
-}
+const loadMealDetail = (idMeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  //console.log(idMeal)
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => desplyMealDetails(data.meals[0]))
+    .catch((error) => {
+      console.log(error);
+    });
+};
+// async
+const loadMealDetail2 = async (idMeal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    desplyMealDetails(data.meals[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const desplyMealDetails = (meal)=>{
- document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
-  const mealsDetails = document.getElementById('mealDetailsBody');
+const desplyMealDetails = (meal) => {
+  document.getElementById("mealDetailsLabel").innerText = meal.strMeal;
+  const mealsDetails = document.getElementById("mealDetailsBody");
   mealsDetails.innerHTML = `
     <h5>Meal id: '${meal.idMeal}'</h5>
     <h5>Category: '${meal.strCategory}'</h5>
-    <h5>Area: '${meal.strArea}'</h5>
-    <h5>Ingredient: '${meal.strIngredient1}'</h5>
-    <h5>Instructions
-    : '${meal.strInstructions
-    }'</h5>
+    <p>Area: '${meal.strArea}'</p>
+    <p>Ingredient: '${meal.strIngredient1}'</p>
+    <p>Instructions
+    : '${meal.strInstructions}'</p>
   
-  `
-}
+  `;
+};
 
-loadMeals('rice');
+loadMeals("chi");
